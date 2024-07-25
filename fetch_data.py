c7085b8a52e3816b82c55d53eb07618c32822095
Swastik Agrawal <swastikagrawal3@gmail.com>
2024-07-25 19:29:42 +0530

    populated mongoDB database with dummy data

---
+++ fetch_data.py
@@ -0,0 +1,25 @@
+'''
+only for one-time usage, to populate my mongdb database, set up using Atlas
+'''
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import requests
+
+# a dummy data api, has 200 products' dummy data
+all_products = requests.get("https://api.escuelajs.co/api/v1/products").json()
+print(all_products)
+
+# removed my mongoDB details
+uri = "mongodb+srv://<username>:<password>@cluster0.7zv8z.mongodb.net/<dbname>?retryWrites=true&w=majority"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+
+    client.amazon.products.insert_many(all_products)
+    print("Inserted product into the database")
+
+except Exception as e:
+    print(e)
