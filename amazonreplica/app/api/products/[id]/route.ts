8748d5ca5ef125f2b0f1af7aa4243f2b67fd5d37
Swastik Agrawal <swastikagrawal3@gmail.com>
2024-07-26 23:04:37 +0530

    removed unnecessary debug logs

--- amazonreplica/app/api/products/[id]/route.ts
+++ amazonreplica/app/api/products/[id]/route.ts
@@ -17,10 +17,10 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
         { status: 400 }
       );
     }
-    // console.log("The product ID is:",  new ObjectId(productId));
+
     // Fetch the specific product
     const product = await em.findOne(Product, { _id: new ObjectId(productId) });
-    // console.log("The product fetched is:", product);
+    console.log("The product fetched is:", product);
     if (!product) {
       return NextResponse.json(
         { error: 'Product not found' },
